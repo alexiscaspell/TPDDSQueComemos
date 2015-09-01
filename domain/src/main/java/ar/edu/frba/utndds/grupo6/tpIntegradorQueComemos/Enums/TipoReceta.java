01be170f0d0c8532943673d9e0dbe8e5c1555f57
lucianoiasi <lucianoiasi03@hotmail.com>
2015-09-01 17:42:39 -0300

    Push Completo de Modificaciones

---
+++ domain/src/main/java/ar/edu/frba/utndds/grupo6/tpIntegradorQueComemos/Enums/TipoReceta.java
@@ -0,0 +1,24 @@
+package ar.edu.frba.utndds.grupo6.tpIntegradorQueComemos.Enums;
+
+
+public enum TipoReceta {
+	PUBLICA
+	{
+		@Override
+		public boolean esPublica()
+		{
+			return true;
+		}
+	},
+	PRIVADA
+	{
+		@Override
+		public boolean esPublica()
+		{
+			return false;
+		}
+		
+	};
+	
+	public abstract boolean esPublica();
+}
